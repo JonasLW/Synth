514d209ae536201feb63d873d3d0b7e88942d55e
Jonas Willadsen <jonas.l.willadsen@gmail.com>
2021-12-17 20:17:06 +0100

    New key input handling
    Handling key inputs with dictionaries. However, this does not seem to
    work as well as desired. May need to find a way to access/set keycodes
    programatically.

--- Synth.py
+++ Synth.py
@@ -1,11 +1,14 @@
 # A file to play programatically created audio with the keyboard
+import tkinter as tk 
 import numpy as np
 import pyaudio
-import tkinter as tk
 import os
 import time
 import matplotlib.pyplot as plt
 from scipy import signal
+from debouncer import Debouncer
+
+# TODO: Debounce manually with a function. xset is linux only
 
 # A BIG FAT COMMENT THAT'S NOT SUPPOSED TO BE IN MAIN BRANCH
 
@@ -15,6 +18,12 @@ BITRATE = 16000     #number of frames per second/frameset.
 BUFFER_LENGTH = 0.02  # Length of buffer in seconds
 BUFFER_FRAMES_NR = int(BITRATE*BUFFER_LENGTH)
 ET_RATIO = 2**(1/12)  # Semitone ratio for equal temperament
+
+# Need to use keycodes instead of key symbols, since capslock modifies key symbols
+# Should be set programatically
+key_dict_scale = {"1":0,"2":1,"3":2,"4":3,"5":4,"6":5,"7":6,"8":7}
+key_dict_chord = {"j":0,"k":1,"l":2,"oslash":3,"ae":4}
+key_dict_misc = {"f":0,"g":1,"Shift_L":2,"Caps_Lock":3}
 
 try:
     # Setting up tkinter
@@ -89,6 +98,8 @@ try:
         global active_freqs
         global root
         global mixed_flag
+        global save_data_0
+        global save_data_1
 
         root.after(5, mixing)
         if mixed_flag:
@@ -110,7 +121,10 @@ try:
                 else:
                     temp_data += np.sin(f*t + phase)
             max_amplitude = (np.amax(np.square(temp_data), initial=1))**0.5
-            temp_data /= max_amplitude
+            temp_data = 0.8*temp_data/max_amplitude
+            if temp_data[100] > 0.5:
+                save_data_0 = np.copy(save_data_1)
+                save_data_1 = np.copy(temp_data)
             data = temp_data.astype(np.float32).tobytes()
             #end = time.perf_counter()
             #print(f"mixed at {end:.5f} \n")
@@ -126,38 +140,12 @@ try:
         global just_released
         global ramp_up
         global ramp_down
-        global save_data_0
-        global save_data_1
         global mixed_flag
         # TODO: Mixing should be done while stream is playing, not after it has called callback
         # Note, if so the phase can not be determined in the mixing stage
 
-        #last_data = data
         #call = time.perf_counter()
         #print(f"calling at {call:.5f} --------------------------------------------\n")
-        """
-        data = np.copy(no_sound)
-        for i, f in enumerate(active_freqs):
-            phase = f*BUFFER_LENGTH*count%(2*np.pi)
-            if just_pressed[i]:
-                data += np.sin(f*t + phase)*ramp_up
-                just_pressed[i] = 0
-            elif just_released[i]:
-                data += np.sin(f*t + phase)*ramp_down
-                just_released[i] = 0
-                active_freqs[i] = 0
-            else:
-                data += np.sin(f*t + phase)
-        max_amplitude = (np.amax(np.square(data), initial=1))**0.5
-        frame = data/max_amplitude
-        """
-        #data = data/max_amplitude
-        #frame = data[0 : frame_count]
-        """
-        if frame[100] > 0.5:
-            save_data_0 = last_data
-            save_data_1 = data
-        """
         #frame = data
         #return_data = frame.astype(np.float32).tobytes()
         return_data = data
@@ -188,38 +176,33 @@ try:
         # Script does not enter this function when pressing 4 or 8 with 3+ chord tones playing
         #    keycodes not working: 13, 17, 18, 31, 32
 
-        if 9 < event.keycode < 18:
+        key = event.keysym
+        if key in key_dict_scale:
             # TODO: Make sure currently playing note changes. Half done. Clicks a little. 4 and 8 not always working
-            scale_degree = event.keycode - 10
+            scale_degree = key_dict_scale[key]
             chord_freqs = set_chord_freqs(scale_degree)
             active_freqs = chord_freqs*(active_freqs > 1)
-        elif event.keycode == 41:
-            # Key: f - flat tone
-            scale = scale/ET_RATIO
-            chord_freqs = set_chord_freqs(scale_degree)
-            active_freqs = chord_freqs*(active_freqs > 1)
-        elif event.keycode == 42:
-            # Key: g - sharp tone
-            scale = scale*ET_RATIO
-            chord_freqs = set_chord_freqs(scale_degree)
-            active_freqs = chord_freqs*(active_freqs > 1)
-        elif 43 < event.keycode < 49:
-            index = event.keycode - 44
+        elif key in key_dict_chord:
+            index = key_dict_chord[key]
             active_freqs[index] = chord_freqs[index]
             just_pressed[index] = 1
-        elif event.keycode == 50:
-            # TODO: Use a better kind of comparison. This is not compatible with the way I'm doing sharps and flats
-            if (scale == major_scale).all():
-                scale = minor_scale
-            elif (scale == minor_scale).all():
-                scale = major_scale
-            chord_freqs = set_chord_freqs(scale_degree)
-            active_freqs = chord_freqs*(active_freqs > 1)
-        elif event.keycode == 66:
-            if (scale == major_scale).all():
-                scale = minor_scale
-            elif (scale == minor_scale).all():
-                scale = major_scale
+        elif key in key_dict_misc:
+            action = key_dict_misc[key]
+            if action == 0:
+                scale = scale/ET_RATIO
+            elif action == 1:
+                scale = scale*ET_RATIO
+            elif action == 2:
+                # TODO: Use a better kind of comparison. This is not compatible with the way I'm doing sharps and flats
+                if (scale == major_scale).all():
+                    scale = minor_scale
+                elif (scale == minor_scale).all():
+                    scale = major_scale
+            elif action == 3:
+                if (scale == major_scale).all():
+                    scale = minor_scale
+                elif (scale == minor_scale).all():
+                    scale = major_scale
             chord_freqs = set_chord_freqs(scale_degree)
             active_freqs = chord_freqs*(active_freqs > 1)
 
@@ -233,30 +216,24 @@ try:
         global scale_degree
         global active_freqs
         global chord_freqs
-
-        if event.keycode == 41:
-            # Key: f - release flat
-            scale = scale*ET_RATIO
-            chord_freqs = set_chord_freqs(scale_degree)
-            active_freqs = chord_freqs*(active_freqs > 1)
-        elif event.keycode == 42:
-            # Key: g - release sharp
-            scale = scale/ET_RATIO
-            chord_freqs = set_chord_freqs(scale_degree)
-            active_freqs = chord_freqs*(active_freqs > 1)
-        elif event.keycode == 50:
-            if (scale == major_scale).all():
-                scale = minor_scale
-            elif (scale == minor_scale).all():
-                scale = major_scale
-            chord_freqs = set_chord_freqs(scale_degree)
-            active_freqs = chord_freqs*(active_freqs > 1)
-
-        index = event.keycode - 44
-        try:
+        
+        key = event.keysym
+        if key in key_dict_chord:
+            index = key_dict_chord[key]
             just_released[index] = 1
-        except:
-            pass
+        elif key in key_dict_misc:
+            action = key_dict_misc[key]
+            if action == 0:
+                scale = scale*ET_RATIO
+            elif action == 1:
+                scale = scale/ET_RATIO
+            elif action == 2:
+                if (scale == major_scale).all():
+                    scale = minor_scale
+                elif (scale == minor_scale).all():
+                    scale = major_scale
+            chord_freqs = set_chord_freqs(scale_degree)
+            active_freqs = chord_freqs*(active_freqs > 1)
 
 
     chord_freqs = set_chord_freqs(0)
@@ -278,8 +255,8 @@ try:
     stream.stop_stream()
     stream.close()
     p.terminate()
-    #plt.plot(t,save_data_0,'r',t,save_data_1,'b')
-    #plt.show()
+    plt.plot(t,save_data_0,'r',t,save_data_1,'b')
+    plt.show()
     os.system('xset r on')
 except:
     print("something went wrong")
